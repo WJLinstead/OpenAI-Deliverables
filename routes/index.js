aeb42e4e34269f50a3109467669176d64f8b9b56
syanismu <113151292+syanismu@users.noreply.github.com>
2024-03-30 21:13:19 -0400

    added env and gitignore. Added simple test in index.js

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,19 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+const OpenAIApi  = require("openai"); 
+require("dotenv").config(); 
+
+  const openai = new OpenAIApi( {
+    apiKey: process.env.OPENAI_SECRET_KEY, }); 
+    
+  async function main() {
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: 'Say this is a test' }],
+      model: 'gpt-3.5-turbo',
+    });
+  }
+  
+  main();
