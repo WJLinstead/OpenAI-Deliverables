16a4c7555756ba2e9c0442c45a845b0d8a9097d1
Jashan <jashan6103@gmail.com>
2024-03-31 17:02:15 -0400

    update gpt version

--- routes/index.js
+++ routes/index.js
@@ -36,7 +36,7 @@ router.post("/ask", async (req, res) => {
         role: "user", 
         content: `Translate: [ ${prompt} ] to ${language}` 
       }],
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
     });
     
     const translation = response.choices[0].message.content;
