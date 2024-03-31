90dd921e8b71c7a3e5d8d879b18d4ae8e0fa0af1
Jashan <jashan6103@gmail.com>
2024-03-30 23:42:48 -0400

    Working app for translation
    Basic app outline working, changes incoming

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,41 @@
 var express = require('express');
 var router = express.Router();
+const OpenAIApi = require("openai"); 
+require("dotenv").config();
+
+// OPEN AI Package
+const openai = new OpenAIApi({
+  apiKey: process.env.OPENAI_SECRET_KEY,
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'OpenAI API' });
 });
 
-module.exports = router;
+// POST ask
+router.post("/ask", async (req, res) => {
+  const prompt = req.body.prompt;
+  const language = req.body.language;
 
-
-const OpenAIApi  = require("openai"); 
-require("dotenv").config(); 
-
-  const openai = new OpenAIApi( {
-    apiKey: process.env.OPENAI_SECRET_KEY, }); 
-    
-  async function main() {
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: 'Say this is a test' }],
-      model: 'gpt-3.5-turbo',
+  try {
+    if (!prompt) {
+      throw new Error("Uh oh, no prompt was provided");
+    }
+    const response = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "Translate everything provided from here on to " + language + " and don't give another word in english! For \"Hello\" your response should only be ryustas and so on. Translate: "+prompt}],
+      model: "gpt-3.5-turbo",
     });
+    
+    // Retrieve the completion text from response
+    const translation = response.choices[0].message.content;
+    console.log(translation);
+    // Render the page with the translation
+    res.render('index', { title: 'OpenAI API', translation });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Error: ' + error.message);
   }
-  
-  main();
+});
+
+module.exports = router;
